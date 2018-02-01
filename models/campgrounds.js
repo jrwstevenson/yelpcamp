eedc26d3a4d4357b06a922cc7a2ef90bf4beba0c
James Stevenson <jrwstevenson@gmail.com>
2018-02-01 17:20:02 +0000

    fix moment time for campgrounds

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -5,7 +5,7 @@ var campgroundSchema = new mongoose.Schema({
    price: Number,
    image: String,
    description: String,
-   createdAt: {type: Date, default: Date.now },
+   createdAt: { type: Date, default: Date.now },
    author: {
       id: {
          type: mongoose.Schema.Types.ObjectId,
